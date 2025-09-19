5ee102b7ca0b3480787d5dfce7db706d8b07566a
Matthew Jon <matty_jones@Matthews-MacBook-Air.local>
2025-09-19 02:08:39 -0400

    homewerk

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cse3150_week_3_hw
+
+I didn't really USE pointers persay. Most of the pointer usage was already implemented into the program, I simply dereferenced them sometimes to access or change the values that the pointers were pointing to. Big example in a lot of declarations of variables. In case 1 when I created the array of characters for the string to be copied, I created it with an asterick to let it be known that this was an array called name of character pointers.
+
+Basically all of the const usage was with the char arrays. When throwing and catching for exceptions however, I was required to catch using a const msg c style character array/string. Was in a bad mood when I found out. I just thought you should know that. The syllabus says that all assignments are autograded so I'm assuming you won't see this but it's odd to ask for a written out explanation of code when you won't even read it. Is it just so we get used to writing README.md's Professor Furuness?
+
+References was briefly used in the parameters of a couple functions to pass the address blajhsagndkjsfmlgvb,. Namely addStudent.
+
+I talked about exceptions earlier, they catch if anything goes awry according to my control flow if statement in each function. in addStudent() it gets thrown in the case that the size exceeds the capacity or equals it.
+
+Control flow was used hand in hand with all exceptions. I also used for loops primarily. There was an opportunity for a while loop but I DIDNT WANT TO!!!
---
+++ solution.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <cstring>   // for strlen, strcpy
+
+// TODO: function prototypes
+void addStudent(char* name, double gpa, char* names[], double gpas[], int& size, int capacity);
+void updateGPA(double* gpaPtr, double newGpa);
+void printStudent(const char* name, const double& gpa);
+double averageGPA(const double gpas[], int size);
+// TODO: implement addStudent
+void addStudent(char* name, double gpa, char* names[], double gpas[], int& size, int capacity) {
+	if (size >= capacity){
+		const char* msg = "Too many students, gotta expel some bro.";
+		throw msg;
+	} else {
+		names[size] = new char[std::strlen(name) + 1];
+		std::strcpy(names[size], name);	
+		gpas[size] = gpa;
+		size++;
+	}
+}
+// TODO: implement updateGPA
+void updateGPA(double* gpaPTR, double newGPA){
+	if (newGPA < 0){
+		const char* msg = "New GPA value invalid.";
+		throw msg;
+	}
+	*gpaPTR = newGPA;
+}
+// TODO: implement printStudent
+void printStudent(const char* name, const double& gpa){
+	if (name == nullptr) {
+		const char* msg = "You got no students lol.";
+		throw msg;
+	} else {
+		std::cout << name << ", " << gpa << std::endl;
+	}
+}
+// TODO: implement averageGPA
+double averageGPA(const double gpas[], int size){
+	if (size <= 0) {
+		const char* msg = "Not enough students to average man.";
+		throw msg;
+	} else {
+		double sum = 0.0;
+		for (int i = 0; i < size; i++){
+			sum += gpas[i];
+		}
+		return sum / static_cast<double>(size);
+	}
+}
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: ./program <capacity>" << std::endl;
+        return 1;
+    }
+
+    int capacity = std::stoi(argv[1]);
+    char* names[capacity];
+    double gpas[capacity];
+    int size = 0;
+	
+	
+	
+    int choice;
+    do {
+        std::cout << "Menu:\n";
+        std::cout << "1. Add student\n";
+        std::cout << "2. Update GPA\n";
+        std::cout << "3. Print all students\n";
+        std::cout << "4. Compute average GPA\n";
+        std::cout << "5. Quit\n";
+        std::cout << "Enter choice: ";
+        std::cin >> choice;
+	
+	
+	// asked claude because my program was looping in on itself, said this line would fix it
+	std::cin.ignore();
+	
+        switch (choice) {
+            case 1: {
+                // TODO: implement menu logic
+		std::cout << "Enter Student Name: " << std::endl;
+		std::string temp;
+		std::getline(std::cin, temp);
+		char *name = new char[std::strlen(temp.c_str()) + 1];
+		std::strcpy(name, temp.c_str());
+		
+		std::cout << "Enter GPA: " <<std::endl;	
+		double gpa;
+		std::cin >> gpa;
+		
+		try {
+			addStudent(name, gpa, names, gpas, size, capacity);
+			delete[] name;
+		} catch(const char* msg) {
+			std::cout << msg << std::endl;
+			delete[] name;
+		}
+		break;
+            	}
+            case 2: {
+                // TODO: implement menu logic
+		std::cout << "Enter Index of GPA to be Changed: " << std::endl;
+		int index;
+		std::cin >> index;
+		if (index > size){
+			std::cout << "Invalid Index." << std::endl;
+			break;
+		}
+		std::cout << "Enter New GPA: " << std::endl;
+            	double newGPA;
+		std::cin >> newGPA;
+		
+		try {
+			updateGPA(&gpas[index], newGPA);
+		} catch (const char* msg) {
+			std::cout << msg << std::endl;
+		}
+		break;
+		}
+            case 3: {
+                // TODO: implement menu logic
+		try {
+			for (int i = 0; i < size; i++){
+				printStudent(names[i], gpas[i]);
+			}
+		} catch(const char* msg) {
+			std::cout << msg << std::endl; 
+		}
+		break;	
+            	}
+            case 4: {
+                // TODO: implement menu logic
+                try {
+			double avg = averageGPA(gpas, size);
+			std::cout << static_cast<int>(avg) << std::endl;
+		} catch(const char* msg) {
+			std::cout << msg << std::endl;
+		}
+		break;
+            }
+            case 5: {
+                std::cout << "Goodbye!" << std::endl;
+                break;
+            }
+            default: {
+                std::cout << "Invalid choice" << std::endl;
+            }
+        }
+    } while (choice != 5);
+
+    // TODO: free memory
+	for (int i = 0; i < size; i++) {
+		delete[] names[i];
+	}
+    return 0;
+}
